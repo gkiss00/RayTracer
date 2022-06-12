673f6d2e7e06e514c9c66f96778dd9e2db5d0cd0
gautier kiss <gkgkgk395@gmail.com>
2022-06-12 22:00:31 +0200

    client ongoing

--- src/main/java/rayTracer/io/Client.java
+++ src/main/java/rayTracer/io/Client.java
@@ -2,9 +2,11 @@ package rayTracer.io;
 
 import rayTracer.lights.Light;
 import rayTracer.objects.BaseObject;
+import rayTracer.utils.Calculator;
 import rayTracer.utils.SceneMaker;
 import rayTracer.visual.Camera;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
@@ -12,12 +14,15 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 
 public class Client {
     private static Camera cam;
     private static List<BaseObject> objects = new ArrayList<>();
     private static List<Light> lights = new ArrayList<>();
     private static List<Thread> threads = new ArrayList<>();
+    private Semaphore mutex = new Semaphore(1);
+
 
     private static Socket socket;
     private static BufferedReader bufferedReader;
@@ -34,7 +39,7 @@ public class Client {
         try {
             socket.close();
         } catch (Exception e) {
-
+            System.out.println(e);
         }
     }
 
@@ -43,10 +48,45 @@ public class Client {
             for (int w = min; w < max; ++ w) {
                 // calculate pixel color
                 // send it to the server
-                printStream.println(w + " " + h + " COLOR"); // <X> <Y> <PIXEL_COLOR>
+                printStream.println(w + " " + h + " #ff0000"); // <X> <Y> <PIXEL_COLOR>
             }
         }
         printStream.println("DISCONNECT");
+        closeConnection();
+    }
+
+    private static void waitThreadsToEnd(int nbThread) {
+        for (int i = 0; i < nbThread; ++i) {
+            try {
+                threads.get(i).join();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void startThreads(int nbThread, rayTracer.config.Config config) {
+        for (int i = 0; i < nbThread; ++i) {
+            Calculator calculator = new Calculator(config, null, nbThread, min + i, max, false);
+            Thread thread = new Thread(calculator);
+            threads.add(thread);
+            thread.start();
+        }
+    }
+
+    private static void startMultiThread(rayTracer.config.Config config){
+        int nbThread = Runtime.getRuntime().availableProcessors();
+        startThreads(nbThread, config);
+        waitThreadsToEnd(nbThread);
+        for (int h = 0; h < config.height; ++h) {
+            for (int w = min; w < max; ++ w) {
+                // calculate pixel color
+                // send it to the server
+                printStream.println(w + " " + h + " #ff0000"); // <X> <Y> <PIXEL_COLOR>
+            }
+        }
+        printStream.println("DISCONNECT");
+        closeConnection();
     }
 
     //***********************************************************************
@@ -89,7 +129,7 @@ public class Client {
         // Server part
         try{
             //Connect to the server
-            socket = new Socket("localhost", 5000);
+            socket = new Socket("192.168.1.61", 5000);
             bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             printStream = new PrintStream(socket.getOutputStream());
             waitForStart();
